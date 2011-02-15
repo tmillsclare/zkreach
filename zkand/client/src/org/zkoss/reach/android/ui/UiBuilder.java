ccd398cdd36688288233fb092203f38e5d174b27
tmillsclare <timothyclare@gmail.com>
2011-02-15 14:57:07 +0800

    Remove all builder information

--- zkand/client/src/org/zkoss/reach/android/ui/UiBuilder.java
+++
@@ -1,7 +0,0 @@
-package org.zkoss.reach.android.ui;
-
-public class UiBuilder {
-	public UiBuilder(String className) {
-		UiManager.registerBuilder(className, this);
-	}
-}
--- zkand/client/src/org/zkoss/reach/android/ui/factory/TextViewBuilder.java
+++
@@ -1,9 +0,0 @@
-package org.zkoss.reach.android.ui.factory;
-
-import org.zkoss.reach.android.ui.UiBuilder;
-
-public class TextViewBuilder extends UiBuilder {
-	public TextViewBuilder() {
-		super("android.widget.TextView");
-	}
-}
